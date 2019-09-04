5b3792a8b4812b0a8e9befbeb365ae55d0d7462b
isaac <Isaacboon@gmail.com>
2019-09-04 14:28:32 +1200

    meshtape corner

--- Assets/Player/Scripts/PlayerDeath.cs
+++ Assets/Player/Scripts/PlayerDeath.cs
@@ -65,16 +65,4 @@ public class PlayerDeath : MonoBehaviour
         PlayerManager.player.GetComponent<Rigidbody>().velocity = new Vector3();
     }
 }
-
-    public void DamagePlayer(float damage) {
-        health = health - damage;
-        CheckHealth();
-    }
-
-    void CheckHealth() { 
-        if(health <= 0) {
-            killPlayer();
-        }
-    }
-
     
